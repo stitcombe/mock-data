0402926564069ba475f5689d3906dcd90cf8a77e
Stephen Titcombe <40187876+stitcombe@users.noreply.github.com>
2023-04-26 03:18:51 +0000

    feat: break functions out

--- src/mock_data/function.py
+++ src/mock_data/function.py
@@ -2,23 +2,59 @@ import hashlib
 import string
 import random
 
+# Define salt value
+salt = "".join(random.choices(string.ascii_letters + string.digits, k=5))
+print("The salt value used is: " + salt)
 
-# Define a function to de-identify a string
-def deidentify_value(value):
+
+# Define a function to de-identify a claim number
+def deid_claim(value):
     if not isinstance(value, str):
         raise AttributeError("Value must be a string.")
-
     if not value:
         return value
-
-    # Define a salt value
-    salt = "".join(random.choices(string.ascii_letters + string.digits, k=5))
     # Add the salt to the original value
     salted_value = salt + value
     # Hash the salted value using SHA-256
     hashed_value = hashlib.sha256(salted_value.encode()).hexdigest()
     # Convert the hexadecimal representation to a string of numeric characters
+    # Truncate at 15 characters
     numeric_value = "".join([str(int(c, 16) % 10) for c in hashed_value])[:15]
     # Add the first two characters from the original value to the de-identified value
     deidentified_value = value[:2] + numeric_value[2:]
+    return deidentified_value
+
+
+# Define a function to de-identify a member id
+def deid_memberId(value):
+    if not isinstance(value, str):
+        raise AttributeError("Value must be a string.")
+    if not value:
+        return value
+    # Add the salt to the original value
+    salted_value = salt + value
+    # Hash the salted value using SHA-256
+    hashed_value = hashlib.sha256(salted_value.encode()).hexdigest()
+    # Add the last 3 characters from the original value
+    # to first n characters of the hashed value,
+    # where n is the length of the input value
+    deidentified_value = hashed_value[: len(value) - 3] + value[-3:]
+    return deidentified_value
+
+
+# Define a function to de-identify a member first or last name
+def deid_memberName(value):
+    if not isinstance(value, str):
+        raise AttributeError("Value must be a string.")
+    if not value:
+        return value
+    # Add the salt to the original value
+    salted_value = salt + value
+    # Hash the salted value using SHA-256
+    hashed_value = hashlib.sha256(salted_value.encode()).hexdigest()
+    # Convert the hashed value to alpha characters only
+    deidentified_value = "".join(filter(str.isalpha, hashed_value))
+    # Extract the first n characters of the hashed value,
+    # where n is the length of the input value
+    deidentified_value = deidentified_value[: len(value)]
     return deidentified_value
--- src/mock_data/main.py
+++ src/mock_data/main.py
@@ -1,11 +1,14 @@
 import json
 import function
-from pathlib import Path
+from root_dir import ROOT_DIR
 
-filepath = Path(__file__).parent / "claim_mock.json"
+# Define `files` folder  path
+files_dir = ROOT_DIR / "files"
+inputFile = files_dir / "claim_mock.json"
+outputFile = files_dir / "output.json"
 
 # Load the JSON file
-with open(filepath, "r") as f:
+with open(inputFile, "r") as f:
     data = json.load(f)
 
 # Loop through each item in the JSON data
@@ -16,12 +19,20 @@ for item in data:
         # Check if the 'claimNumber' key is present in the claim dictionary
         if "claimNumber" in claim:
             # De-identify the value of the 'claimNumber' key
-            claim["claimNumber"] = function.deidentify_value(claim["claimNumber"])
+            claim["claimNumber"] = function.deid_claim(claim["claimNumber"])
         # Check if the 'memberId' key is present in the claim dictionary
         if "memberId" in claim:
             # De-identify the value of the 'memberId' key
-            claim["memberId"] = function.deidentify_value(claim["memberId"])
+            claim["memberId"] = function.deid_memberId(claim["memberId"])
+        # Check if the 'firstName' key is present in the claim dictionary
+        if "firstName" in claim:
+            # De-identify the value of the 'firstName' key
+            claim["firstName"] = function.deid_memberName(claim["firstName"])
+        # Check if the 'lastName' key is present in the claim dictionary
+        if "lastName" in claim:
+            # De-identify the value of the 'firstName' key
+            claim["lastName"] = function.deid_memberName(claim["lastName"])
 
 # Write the updated JSON data to a file
-with open("your_updated_file.json", "w") as f:
+with open(outputFile, "w") as f:
     json.dump(data, f)
